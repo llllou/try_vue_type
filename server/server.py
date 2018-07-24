66710e32912e872ac9edeb699c52946def91ba23
wanghuaizhi <llllou114@gmail.com>
2018-07-24 16:15:10 +0800

    new

--- server/server.py
+++ server/server.py
@@ -1,15 +1,18 @@
-#-w- coding: utf-8 -w-
+# -w- coding: utf-8 -w-
 
 from flask import Flask
-from flask import request,render_template
+from flask import request, render_template
 from utils.specialNum import fib
+from io import StringIO
+import time
 import logging
-
 app = Flask(__name__)
 # todo:生产模式时取消debugger
 app.debug = True
 a = fib(10)
-@app.route('/',methods = ['GET','POST','DOC'])
+
+
+@app.route('/', methods=['GET', 'POST', 'DOC'])
 def index():
     return render_template('index.html');
 
@@ -20,6 +23,14 @@ def fibHandler():
         if i > 30:
             b = i
             break;
-    return '<h3>number is %s</h3>' % b
+    return '<h3>time:%a \n number is %s</h3>' % (time.asctime(time.time()), b)
 
-app.run(port = 8080,host = 'localhost');
+@app.route('/sys')
+def sysHandler():
+    with open('C:\Windows\system.ini', 'r', encoding='gbk') as f:
+        b = f.read();
+        print(b)
+    return '<div>%a</div>' % b
+
+if __name__ == '__main__':
+  app.run(port=8080, host='localhost');
